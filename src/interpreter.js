130edeb9432ff26387d95f81a6577a2c3272b1b6
pellarolojuani <pellarolojuani@gmail.com>
2017-10-01 18:19:35 -0300

    algunos avances..

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,11 +1,57 @@
 var Interpreter = function () {
 
-    this.parseDB = function (params, paramss, paramsss) {
+    var db;
 
+    this.validateFactSyntax = function (query) {
+        var result = true;
+        if (query === "") {
+            result = false;
+        } else if (query.indexOf("(") === -1) {
+            result = false;
+        } else if (query.indexOf(")") === -1) {
+            result = false;
+        }
+        return result;
+    }
+
+    this.getFactsFromDatabase = function (database) {
+        var result = [];
+        for (var i = 0; i < database.length; i++) {
+            var line = database[i].trim();
+            var parsedLine = line.replace("\.", "");
+            if (parsedLine.indexOf(":-") === -1) {
+                result.push(parsedLine);
+            }
+        }
+        return result;
+    }
+    
+    this.getRulesFromDatabase = function (database) {
+        var result = [];
+        for (var i = 0; i < database.length; i++) {
+            var line = database[i].trim();
+            var parsedLine = line.replace("\.", ""); 
+            if (parsedLine.indexOf(":-") !== -1) {
+                result.push(parsedLine);
+            }
+        }
+        return result;
+    }
+    
+    this.analizeFacts = function (parsedFacts, query) {
+        
+    } 
+
+    this.parseDB = function (database) {
+        //cargo la database en db!
     }
 
     this.checkQuery = function (params) {
-        return true;
+        var database = this.parseDB()
+        if (validateFactSyntax(query)) {
+            return evaluateQuery(query);
+        }
+        return null;
     }
 
 }
