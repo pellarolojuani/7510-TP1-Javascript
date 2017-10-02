e1fb636043a4fe4c0c77c4e4bbd7d14a42a113a1
pellarolojuani <pellarolojuani@gmail.com>
2017-10-01 23:15:38 -0300

    mas cosas

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,5 +1,4 @@
 var Interpreter = function () {
-
     var db;
 
     this.validateFactSyntax = function (query) {
@@ -25,25 +24,47 @@ var Interpreter = function () {
         }
         return result;
     }
-    
+
     this.getRulesFromDatabase = function (database) {
         var result = [];
         for (var i = 0; i < database.length; i++) {
             var line = database[i].trim();
-            var parsedLine = line.replace("\.", ""); 
+            var parsedLine = line.replace("\.", "");
             if (parsedLine.indexOf(":-") !== -1) {
                 result.push(parsedLine);
             }
         }
         return result;
     }
-    
+
     this.analizeFacts = function (parsedFacts, query) {
-        
-    } 
+        var result = false;
+        for (var i = 0; i < parsedFacts.length; i++) {
+            if (parsedFacts[i] === query) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    this.getRuleNameFromQuery = function (query) {
+        return query.substring(0, query.indexOf("("));
+    }
+
+    this.getRuleByName = function (parsedRules, ruleName) {
+        var rule = null;
+        for (var i = 0; i < parsedRules.length; i++) {
+            if (getRuleNameFromQuery(parsedRules[i]) === ruleName) {
+                rule = parsedRules[i];
+                break;
+            }
+        }
+        return rule;
+    }
 
     this.parseDB = function (database) {
-        //cargo la database en db!
+        this.db = database;
     }
 
     this.checkQuery = function (params) {
